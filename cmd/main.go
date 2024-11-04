254ac71228dfb6e447a67e7e03e96102a058b545
Thomas Maffezzolli <thomasmaffezzolli8@gmail.com>
2024-11-04 18:50:39 -0300

    ✨ Adição da conexão ao mongo

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"lab-manager-api/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	config.ConnectDB()
 }
---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Client
+
+func ConnectDB() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connecting to MongoDB...")
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	DB = client
+}
