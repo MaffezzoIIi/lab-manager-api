e80c0677361e2547c8508fcee99d313866fa8551
Thomas Maffezzolli <thomasmaffezzolli8@gmail.com>
2024-11-13 20:01:55 -0300

    ✨ Adição do modelo do usuario e criacao

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"lab-manager-api/config"
+	"lab-manager-api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+type CreateUserRequest struct {
+	Name     string          `json:"name" binding:"required"`
+	UserType models.UserType `json:"user_type" binding:"required"`
+	Password string          `json:"password" binding:"required"`
+}
 
 func main() {
 	config.ConnectDB()
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Server is running!")
+	})
+
+	r.POST("/create-user", createUserHandler)
+
+	r.Run(":8080")
+}
+
+func createUserHandler(c *gin.Context) {
+	var req CreateUserRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := models.HashPassword(req.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	user := models.User{
+		Name:     req.Name,
+		UserType: req.UserType,
+		Password: hashedPassword,
+	}
+
 }
---
+++ models/user.go
@@ -0,0 +1,35 @@
+package models
+
+import "golang.org/x/crypto/bcrypt"
+
+type UserType int
+
+const (
+	Admin UserType = iota
+	Professor
+)
+
+type User struct {
+	Name     string
+	UserType UserType
+	Password string
+}
+
+func NewUser(id int, name string, userType UserType, password string) (User, error) {
+	hasedPassword, err := HashPassword(password)
+	if err != nil {
+		return User{}, err
+	}
+
+	return User{Name: name, UserType: userType, Password: hasedPassword}, nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
